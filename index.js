077fdafeed89812d501bdf4ee62c5be3f0ed5dcf
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-08-31 00:11:22 +0700

    add insert and getDataById

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 
+app.use(express.json());
+
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -16,8 +18,53 @@ conn.connect((err) => {
 
 // Menampilkan semua data
 app.get('/api/produk', (req, res) => {
-  let query = conn.query('SELECT * FROM product', (err, result) => {
-    res.send({ status: 'Sukses', result: result });
+  conn.query('SELECT * FROM product', (err, result) => {
+    if (err) {
+      res.send({ status: 'Gagal', message: err.message });
+    } else {
+      res.send({ status: 'Sukses', result });
+    }
+  });
+});
+
+// Menampilkan bedasarkan ID
+app.get('/api/produk/:id', (req, res) => {
+  conn.query(
+    `SELECT * FROM product WHERE product_id=${req.params.id}`,
+    (err, result) => {
+      if (err) {
+        res.send({ status: 'Gagal', message: err.name });
+      } else {
+        res.send({ status: 'Sukses', result });
+      }
+    }
+  );
+});
+
+// Menambahkan data
+app.post('/api/produk', (req, res) => {
+  let data = {
+    product_name: req.body.product_name,
+    product_price: req.body.product_price,
+  };
+  conn.query(`INSERT INTO product SET ?`, data, (err, result) => {
+    if (err) {
+      res.send({ status: 'Gagal', message: err.name });
+    } else {
+      res.send({ status: 'Sukses', result });
+    }
+  });
+});
+
+// Update Data
+app.put('/api/produk/:id', (req, res) => {
+  let sql = `UPDATE product SET product_name='"${req.body.product_name}"', product_price='"${req.body.product_price}"' WHERE product_id="${req.params.id}";`;
+  conn.query(sql, (err, result) => {
+    if (err) {
+      res.send({ status: 'Gagal', message: err.name });
+    } else {
+      res.send({ status: 'Sukses', result });
+    }
   });
 });
 
